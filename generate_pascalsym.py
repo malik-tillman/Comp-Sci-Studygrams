7729855ed1eaf421fdf2a14360aa5ccfbe58ac61
malik-tillman <contact@maliktillman.com>
2019-05-03 18:12:47 -0400

    Added script to print symmetrical pascal matrix of n x n

---
+++ generate_pascalsym.py
@@ -0,0 +1,19 @@
+# Program to print an (n x n) symmetrical pascal matrix
+# By: Malik Tillman
+from pprint import pprint
+
+
+def pascal_sym(n):
+    """" Creates a n x n symmetrical pascal matrix """
+    matrix = [[1] * n for _ in range(n)]  # Initialize n x n matrix of 1s
+
+    for row in range(1, n):
+        """" Loops n rows"""
+        for col in range(1, n):
+            """" Loops n columns"""
+            matrix[row][col] = matrix[row-1][col] + matrix[row][col-1]  # Calculates A(r,c) -> A(r-1,c) + A[r][c-1]
+
+    return matrix
+
+
+pprint(pascal_sym(10))
